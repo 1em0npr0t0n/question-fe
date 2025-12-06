2dfc90a84ffff6f501597f23a6eaff65418a152b
1em0npr0t0n <loseappetite@gmail.com>
2025-12-06 23:47:16 +0800

    fix: 修正用户输入换行符在 画布组件Paragraph里不换行的问题，<br />

--- src/components/questionComponents/questionParagraph/Component.tsx
+++ src/components/questionComponents/questionParagraph/Component.tsx
@@ -3,10 +3,22 @@ import { QuestionParagraphDefaultPropsType, QuestionParagraphPropsType } from '.
 import { Typography } from 'antd';
 const { Paragraph } = Typography;
 const Component: FC<QuestionParagraphPropsType> = (props: QuestionParagraphPropsType) => {
-  const { text, isCenter } = { ...QuestionParagraphDefaultPropsType, ...props };
+  const { text = '', isCenter = false } = { ...QuestionParagraphDefaultPropsType, ...props };
+  //换行符换成 html br 错误示范，存在凭证盗用问题
+  //const htmlBR = text.replaceAll('\n', '<br>');
+  //text切割成数组
+  const textList = text.split('\n');
   return (
     <Paragraph style={{ textAlign: isCenter ? 'center' : 'start', marginBottom: 0 }}>
-      {text}
+      {/* <span dangerouslySetInnerHTML={{ __html: htmlBR }}></span> */}
+      {textList.map((text, index) => {
+        return (
+          <span key={index}>
+            {index > 0 && <br />}
+            {text}
+          </span>
+        );
+      })}
     </Paragraph>
   );
 };
