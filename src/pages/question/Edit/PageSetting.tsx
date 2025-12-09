5040312157112dc0ab1c2201a7e9842499997c71
一点都不馋 <loseappetite@gmail.com>
2025-12-09 11:30:31 +0800

    feat: 增加右侧页面信息版面

--- src/pages/question/Edit/PageSetting.tsx
+++ src/pages/question/Edit/PageSetting.tsx
@@ -1,12 +1,21 @@
-import { Form } from 'antd';
-import { FC } from 'react';
+import { Form, Input } from 'antd';
+import { FC, useEffect } from 'react';
 import useGetPageInfo from '../../../hooks/useGetPageInfo';
+import { useDispatch } from 'react-redux';
+import { resetPageInfo } from '../../../store/pageInfoReducer';
 
+const { TextArea } = Input;
 const PageSetting: FC = () => {
   const pageInfo = useGetPageInfo();
+  const dispatch = useDispatch();
   const [form] = Form.useForm();
+
+  useEffect(() => {
+    form.setFieldsValue(pageInfo);
+  }, [pageInfo, form]);
   function handleValuesChange() {
-    console.log('newProps', form.getFieldsValue());
+    //console.log('newProps', form.getFieldsValue());
+    dispatch(resetPageInfo(form.getFieldsValue()));
   }
   return (
     <Form
@@ -14,7 +23,20 @@ const PageSetting: FC = () => {
       form={form}
       initialValues={pageInfo}
       onValuesChange={handleValuesChange}
-    ></Form>
+    >
+      <Form.Item label="问卷标题" name="title" rules={[{ required: true, message: '请输入标题' }]}>
+        <Input placeholder="请输入标题..." />
+      </Form.Item>
+      <Form.Item label="问卷描述" name="desc">
+        <TextArea placeholder="问卷描述..." />
+      </Form.Item>
+      <Form.Item label="css" name="cssCode">
+        <TextArea placeholder="输入css样式代码..." />
+      </Form.Item>
+      <Form.Item label="js" name="jsCode">
+        <TextArea placeholder="输入js脚本代码..." />
+      </Form.Item>
+    </Form>
   );
 };
 export default PageSetting;
